a633356f7a061a9a140320d5476981b4cf51e8a5
zeenayouhan <zinayouhan33@gmail.com>
2022-03-01 07:25:32 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+import 'package:country_codes/country_codes.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await CountryCodes.init();
+  runApp(CountryCodesExampleApp());
+}
+
+class CountryCodesExampleApp extends StatelessWidget {
+  TableRow _buildEntry({required String title, required String description}) {
+    return TableRow(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(right: 5.0),
+          child: Text(
+            title,
+            textAlign: TextAlign.end,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 5.0),
+          child: Text(description),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Country codes example app'),
+        ),
+        body: Builder(builder: (context) {
+          CountryDetails details = CountryCodes.detailsForLocale();
+          Locale locale = CountryCodes.getDeviceLocale()!;
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Table(
+                    border: const TableBorder(
+                      horizontalInside: BorderSide(width: 0.5),
+                      verticalInside: BorderSide(width: 0.5),
+                      top: BorderSide(),
+                      bottom: BorderSide(),
+                      left: BorderSide(),
+                      right: BorderSide(),
+                    ),
+                    children: <TableRow>[
+                      _buildEntry(
+                          title: 'Device region',
+                          description:
+                              '${locale.languageCode}-${locale.countryCode}'),
+                      _buildEntry(
+                          title: 'Name', description: '${details.name}'),
+                      _buildEntry(
+                          title: 'Localized (PT lang)',
+                          description: '${details.localizedName}'),
+                      _buildEntry(
+                          title: 'Alpha 2',
+                          description: '${details.alpha2Code}'),
+                      _buildEntry(
+                          title: 'Dial code',
+                          description: '${details.dialCode}'),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 200.0,
+                  child: TextFormField(
+                    keyboardType: TextInputType.phone,
+                    inputFormatters: [DialCodeFormatter()],
+                  ),
+                ),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
